fea8bc61338e966cc6d0f25a1bfe7415b8f67b16
ladcs <hpladcs@gmail.com>
2021-05-02 15:16:20 -0300

    requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,8 +18,51 @@ function techList(tecName, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function testIntervalo(key, numeroTel) {
+  if (numeroTel[key] < 0 || numeroTel[key] > 9) {
+    return false;
+  }
+}
+
+function compara(key, repeti, numeroTel) {
+  if (numeroTel[key] === numeroTel[repeti]) {
+    return 1;
+  }
+  return 0;
+}
+
+function testRepeticao(key, numeroTel) {
+  let repetido = 0;
+  for (let repeti = key + 1; repeti < numeroTel.length; repeti += 1) {
+    repetido += compara(key, repeti, numeroTel);
+    if (repetido >= 2) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function testNumValido(numeroTel) {
+  for (let key = 0; key < numeroTel.length; key += 1) {
+    if (testIntervalo(key, numeroTel) === false) {
+      return false;
+    }
+    if (testRepeticao(key, numeroTel) === false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function generatePhoneNumber(T) {
   // seu código aqui
+  if (T.length === 11) {
+    if (testNumValido(T) === true) {
+      return `(${T[0]}${T[1]}) ${T[2]}${T[3]}${T[4]}${T[5]}${T[6]}-${T[7]}${T[8]}${T[9]}${T[10]}`;
+    }
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return 'Array com tamanho incorreto.';
 }
 
 // Desafio 12
